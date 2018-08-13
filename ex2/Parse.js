21df8b1492f46601e4aa7072341b33c465ca0a91
Eitan Sternlicht <eitanoffun@gmail.com>
2018-08-13 05:12:15 +0300

    refactored into simpler design

--- ex2/Parse.js
+++ ex2/Parse.js
@@ -8,29 +8,32 @@ function removeCase(text) {
 function isLetter(c) {
     return c.match(/[a-z|A-Z]/i);
 }
-function first(arr) {
-    return arr[0];
-}
-function second(arr) {
-    return arr[1];
-}
-function stringCompare(s1, s2) {
-    if (s1 > s2)
+function wordCompare(pairA, pairB) {
+    if (pairA[0] > pairB[0])
         return 1;
-    if (s1 < s2)
+    if (pairA[0] < pairB[0])
         return -1;
     return 0;
 }
-function numCompare(n1, n2) {
-    return n1 - n2;
+function countCompare(pairA, pairB) {
+    return pairA[1] - pairB[1];
 }
-
 function identity(a) {
     return a;
 }
 function compose(f, g) {
     return function (a) {
         return f(g(a));
+    }
+}
+function multiLevelCompare(listOfCompares) {
+    return function (a, b) {
+        for (i = 0; i < listOfCompares.length - 1; i++) {
+            const comparison = listOfCompares[i](a, b);
+            if (comparison !== 0) // a != b
+                return comparison;
+        }
+        return listOfCompares[listOfCompares.length - 1](a, b);
     }
 }
 function parseText(text, caseSensitive = true, puncSensitive = true) {
@@ -53,18 +56,14 @@ function parseText(text, caseSensitive = true, puncSensitive = true) {
 
     return Array.from(hashmap.entries());
 }
-function accessAndCompareByTwo(firstCompare, secondCompare, firstAccess, secondAccess) {
-    return function (a, b) {
-        return firstCompare(firstAccess(a), firstAccess(b)) || secondCompare(secondAccess(a), secondAccess(b));
-    }
-}
 function sortText(arr, word) {
     if (word)
-        return arr.sort(accessAndCompareByTwo(stringCompare, numCompare, first, second));
-    return arr.sort(accessAndCompareByTwo(numCompare, stringCompare, second, first));
+        return arr.sort(multiLevelCompare([wordCompare, countCompare]));
+    return arr.sort(multiLevelCompare([countCompare, wordCompare]));
+
 }
 
 
 // MAIN
 const arr = parseText("He saw her walking, so he asked her.", false, false);
-console.log(sortText(arr, false));
+console.log(sortText(arr, true));
