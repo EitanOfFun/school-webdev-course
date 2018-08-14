b0bab3a6375a9a59a854978d6133130f7dfbf8bf
Eitan Sternlicht <eitanoffun@gmail.com>
2018-08-14 18:15:25 +0300

    finishing touches

--- ex2/Parse.js
+++ ex2/Parse.js
@@ -1,10 +1,21 @@
-
+// Functional utils
+function identity(a) {
+    return a;
+}
+function compose(f, g) {
+    return function (a) {
+        return f(g(a));
+    }
+}
+// removes any characters that are NOT letters
 function removePunc(text) {
     return text.split('').filter(c => isLetter(c)).join('');
 }
+// makes all letters lowercase
 function removeCase(text) {
     return text.toLowerCase();
 }
+// returns true if character is a letter
 function isLetter(c) {
     return c.match(/[a-z|A-Z]/i);
 }
@@ -18,17 +29,11 @@ function wordCompare(pairA, pairB) {
 function countCompare(pairA, pairB) {
     return pairA[1] - pairB[1];
 }
-function identity(a) {
-    return a;
-}
-function compose(f, g) {
-    return function (a) {
-        return f(g(a));
-    }
-}
+// returns a compare function from a list of compare functions
+// comparing a and b first based on the first one, if equal on first, based on second, etc...
 function multiLevelCompare(listOfCompares) {
     return function (a, b) {
-        for (i = 0; i < listOfCompares.length - 1; i++) {
+        for (var i = 0; i < listOfCompares.length - 1; i++) {
             const comparison = listOfCompares[i](a, b);
             if (comparison !== 0) // a != b
                 return comparison;
@@ -36,34 +41,31 @@ function multiLevelCompare(listOfCompares) {
         return listOfCompares[listOfCompares.length - 1](a, b);
     }
 }
-function parseText(text, caseSensitive = true, puncSensitive = true) {
-    let sanitize = identity;
-    if (!caseSensitive)
-        sanitize = compose(removeCase, sanitize);
-    if (!puncSensitive)
-        sanitize = compose(removePunc, sanitize);
-
-    const hashmap = new Map();
-
-    text.split(' ')
-        .forEach(word => {
-            const sWord = sanitize(word);
-            if (hashmap.has(sWord))
-                hashmap.set(sWord, hashmap.get(sWord) + 1);
-            else
-                hashmap.set(sWord, 1);
-        });
-
-    return Array.from(hashmap.entries());
+// composes a new sanitization function from a map of functions to booleans telling if we should add that function
+function createSanitizationFunction(mapOfSanitization) {
+    let sanitizationFunc = identity;
+    mapOfSanitization.forEach(function(useSanitizeFunc, sanitizeFunc) {
+        if (useSanitizeFunc)
+            sanitizationFunc = compose(sanitizeFunc, sanitizationFunc);
+    });
+    return sanitizationFunc;
 }
+// Question 1
+function parseText(text, caseSensitive = true, puncSensitive = true) {
+    const sanitizationFunc = createSanitizationFunction(new Map([[removeCase, !caseSensitive], [removePunc, !puncSensitive]]));
+    const hashmap = {};
+    text.split(' ').forEach(word => {
+        const sWord = sanitizationFunc(word);
+        if (sWord in hashmap)
+            hashmap[sWord]++;
+        else
+            hashmap[sWord] = 1;
+    });
+    return Object.entries(hashmap);
+}
+// Question 2
 function sortText(arr, word) {
     if (word)
         return arr.sort(multiLevelCompare([wordCompare, countCompare]));
     return arr.sort(multiLevelCompare([countCompare, wordCompare]));
-
 }
-
-
-// MAIN
-const arr = parseText("He saw her walking, so he asked her.", false, false);
-console.log(sortText(arr, true));
